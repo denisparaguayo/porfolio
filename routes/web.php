3fa132268dd7c3004484f9598160df0980682e70
denisparaguayo <72168757+denisparaguayo@users.noreply.github.com>
2021-05-11 00:08:55 -0400

    crud

--- routes/web.php
+++ routes/web.php
@@ -19,4 +19,6 @@ Route::get('/', function () {
 
 Auth::routes();
 
+Route::resource('/proyectos', App\Http\Controllers\ProyectoController::class);
+
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
